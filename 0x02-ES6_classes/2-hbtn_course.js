09fa95cd701fbb0f83ff88498a39550c1b2258eb
damian oguche <damianguch@yahoo.com>
2023-12-01 04:01:39 +0100

    2-hbtn_course.js file modified

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,43 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('name must be a String');
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    if (!Array.isArray(students)) throw TypeError('students must be an Array');
+    students.forEach((student) => {
+      if (typeof student !== 'string') throw TypeError('student must be a String');
+    });
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // name
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('name must be a String');
+    this._name = newName;
+  }
+
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('length must be a Number');
+    this._length = newLength;
   }
 
-  // length
   get length() {
     return this._length;
   }
 
-  set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) throw TypeError('students must be an Array');
+    newStudents.forEach((student) => {
+      if (typeof student !== 'string') throw TypeError('student must be a String');
+    });
+    this._students = newStudents;
   }
 
-  // students
   get students() {
     return this._students;
   }
-
-  set students(students) {
-    if (
-      Array.isArray(students)
-      && students.every((s) => typeof s === 'string')
-    ) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
-  }
 }
-
-export default HolbertonCourse;
